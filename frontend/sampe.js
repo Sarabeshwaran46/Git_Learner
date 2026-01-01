3b6257c8047bacc23b6a7bac81480fc03d3f7df0
Sarabeshwaran46 <sarabesh348@gmail.com>
2026-01-01 15:09:43 +0530

    hello

---
+++ frontend/sampe.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo">This is where the JavaScript output will go.</p>
+
+<script>
+document.getElementById("demo").innerHTML = "Hello World!";
+</script>
+
+</body>
+</html>
